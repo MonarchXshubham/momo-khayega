feefeaa6e3f0b2d38ea8719074942e159e8a136f
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-08-30 13:17:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,8 +111,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://{}/".format(FQDN, PORT)
+URL = "https://thin-marianna-satorugete-16679dda.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://thin-marianna-satorugete-16679dda.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'JisshuBot'))
